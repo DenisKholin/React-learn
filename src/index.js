9792a8d23431a0dc830a4d73578dd269c3086c70
Denis Kholin <den07.06@yandex.ru>
2022-11-11 12:58:26 +0300

    create repository

--- src/index.js
+++ src/index.js
@@ -1,17 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
+const Header = () => {
+	return <h1>HELLO MOTHER FUCKER</h1>;
+}
+
+const Input = () => {
+	return <input type="text" placeholder='WHAT U WANT?' />;
+}
+
+const Button = () => {
+	return <button>LETS GO</button>;
+}
+
+const App = () => {
+	return (<>
+		<Header />
+		<Input />
+		<Button />
+	</>)
+}
+
+root.render(<App />);
+
 reportWebVitals();
